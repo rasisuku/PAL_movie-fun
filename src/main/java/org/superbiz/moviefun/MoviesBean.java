4128766c270fdf189ff6cff2413602eca190d89a
gsomapoornan <ganesh.poorna86@gmail.com>
2018-09-26 16:31:37 +0530

    Final Working-with web, persistence xml

--- src/main/java/org/superbiz/moviefun/MoviesBean.java
+++ src/main/java/org/superbiz/moviefun/MoviesBean.java
@@ -16,7 +16,10 @@
  */
 package org.superbiz.moviefun;
 
-import javax.ejb.Stateless;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
@@ -28,13 +31,14 @@ import java.util.List;
 @Repository
 public class MoviesBean {
 
-    @PersistenceContext(unitName = "movie-unit")
+    @PersistenceContext //(unitName = "movie-unit")
     private EntityManager entityManager;
 
     public Movie find(Long id) {
         return entityManager.find(Movie.class, id);
     }
 
+    @Transactional
     public void addMovie(Movie movie) {
         entityManager.persist(movie);
     }
